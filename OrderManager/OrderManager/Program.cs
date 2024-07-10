9cf1da56c0018f79299cbeb875979ae4274d8f89
Daniil <d.v.larin007@gmail.com>
2024-07-10 21:23:51 +0300

    Исправил код

--- OrderManager/OrderManager/Program.cs
+++ OrderManager/OrderManager/Program.cs
@@ -1,34 +1,47 @@
-﻿static void Inform()
+﻿class Program
 {
-    Console.WriteLine("O V O N");
-
-    Console.Write("Enter your name: ");
-    string name = Console.ReadLine();
-    Console.Write($"Hello {name}! ");
-
-    Console.Write("Enter your product: ");
-    string product = Console.ReadLine();
-
-    Console.Write("Enter count your product: ");
-    string count = Console.ReadLine();
-
-    Console.Write("Enter your address: ");
-    string address = Console.ReadLine();
-
-    Console.WriteLine($"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно?");
-    Check(name, product, count, address);
-}
-Inform();
-static void Check(string name, string product, string count, string address)
-{
-    string check = Console.ReadLine();
-    if (check == "Да" || check == "Yes")
+    static void Main()
     {
-        Console.WriteLine($"{name}!Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address}  к 5 июля");
+        Console.WriteLine( "O Z O N" );
+
+        string name = GetInput( "Enter your name: " );
+        Console.Write( $"Hello {name}! " );
+        string product = GetInput( "Enter your product: " );
+        int count = Convert.ToInt16( GetInput( "Enter count your product: " ) );
+        string address = GetInput( "Enter your address: " );
+
+        Console.WriteLine( $"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно?" );
+        Check( name, product, count, address );
     }
-    else
+    static void Check( string name, string product, int count, string address )
     {
-        Inform();
+        string check = Console.ReadLine().ToLower(); ;
+        if (check == "да" || check == "yes")
+        {
+            DateTime now = DateTime.Now;
+            int todays_date = now.Day;
+            Console.WriteLine( $"{name}, Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address} к {todays_date + 3} июля." );
+        }
+        else
+        {
+            Main();
+        }
+    }
+    static string GetInput( string message )
+    {
+        string input;
+        do
+        {
+            Console.Write( message );
+            input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty( input ))
+            {
+                Console.WriteLine( "Value is not recognized" );
+            }
+        } while (string.IsNullOrEmpty( input ));
+
+        return input;
     }
 }
 
